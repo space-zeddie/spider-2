969c9f77ec511a95c027a6bb14432b0af39acb6f
mzakharuk <mzakharuk@cloudlinux.com>
2017-05-07 11:11:19 +0300

    base implementation

--- src/main/java/base/spider/ResearchPageProcessor.java
+++ src/main/java/base/spider/ResearchPageProcessor.java
@@ -1,7 +1,21 @@
 package base.spider;
 
+import base.PageProcessor;
+import base.web.Page;
+import base.web.Site;
+
 /**
  * Created by matvii on 07.05.17.
  */
-public class ResearchPageProcessor {
+public class ResearchPageProcessor implements PageProcessor {
+    
+    @Override
+    public void process(Page page) {
+
+    }
+
+    @Override
+    public Site getSite() {
+        return null;
+    }
 }
