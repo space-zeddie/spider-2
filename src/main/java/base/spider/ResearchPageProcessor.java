61e59d81ad138b525da9728834bc47077809d189
mzakharuk <mzakharuk@cloudlinux.com>
2017-05-07 11:14:40 +0300

    added Site to RPP

--- src/main/java/base/spider/ResearchPageProcessor.java
+++ src/main/java/base/spider/ResearchPageProcessor.java
@@ -8,7 +8,9 @@ import base.web.Site;
  * Created by matvii on 07.05.17.
  */
 public class ResearchPageProcessor implements PageProcessor {
-    
+
+    private Site site = Site.instance().setRetryTimes(3).setSleepTime(1000).setTimeOut(10000);
+
     @Override
     public void process(Page page) {
 
