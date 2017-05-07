37ed163a909e9cef4d4e7ac97d20313618f9a3b2
mzakharuk <mzakharuk@cloudlinux.com>
2017-05-07 17:45:29 +0300

    had error in regex

--- src/main/java/base/spider/ResearchPageProcessor.java
+++ src/main/java/base/spider/ResearchPageProcessor.java
@@ -18,12 +18,19 @@ public class ResearchPageProcessor implements IPageProcessor {
 
     @Override
     public void process(Page page) {
+        String charset = page.getHtml().$("meta", "charset").get();
+
+        System.out.println("here 1 " + charset);
         // higher priority to .pdf links, since they most likely contain the papers themselves
-        page.addTargetRequests(page.getHtml().links().regex("https://*\\.pdf").all(), 10);
-        page.addTargetRequests(page.getHtml().links().regex("http://*\\.pdf").all(), 10);
+        page.addTargetRequests(page.getHtml().links().regex("https://.*\\.pdf").all(), 10);
+        page.addTargetRequests(page.getHtml().links().regex("http://.*\\.pdf").all(), 10);
+       // for (String link : page.getHtml().links().all()) {
+            //System.out.println(link);
+        //}
+        //System.out.println("");
         // TO DO: non-pdf linksg
 
-        page.putField("paper", page.getUrl().regex("https://*\\.pdf").toString());
+        page.putField("paper", page.getUrl().regex("https://.*\\.pdf").toString());
         page.putField("links", page.getHtml().$("a", "href").all());
         if (page.getResultItems().get("paper")==null || page.getResultItems().get("links")==null
                 || ((List<String>)page.getResultItems().get("links")).size()==0)
