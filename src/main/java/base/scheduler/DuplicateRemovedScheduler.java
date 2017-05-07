52945e0a7767270034dc8509edf184fdd4378753
mzakharuk <mzakharuk@cloudlinux.com>
2017-05-07 15:57:29 +0300

    forgot to commit while writing the scheduler because i'm an idiot

---
+++ src/main/java/base/scheduler/DuplicateRemovedScheduler.java
@@ -0,0 +1,50 @@
+package base.scheduler;
+
+import base.Task;
+import base.scheduler.component.DuplicateRemover;
+import base.scheduler.component.HashSetDuplicateRemover;
+import base.web.Request;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Output that can collect and store results.
+ *
+ * Created by matvii on 11.04.17.
+ */
+public abstract class DuplicateRemovedScheduler implements Scheduler {
+
+    protected Logger logger = LoggerFactory.getLogger(getClass());
+
+    private DuplicateRemover duplicatedRemover = new HashSetDuplicateRemover();
+
+    public DuplicateRemover getDuplicateRemover() {
+        return duplicatedRemover;
+    }
+
+    public DuplicateRemovedScheduler setDuplicateRemover(DuplicateRemover duplicatedRemover) {
+        this.duplicatedRemover = duplicatedRemover;
+        return this;
+    }
+
+    @Override
+    public void push(Request request, Task task) {
+        logger.trace("get a candidate url {}", request.getUrl());
+        if (shouldReserved(request) || noNeedToRemoveDuplicate(request) || !duplicatedRemover.isDuplicate(request, task)) {
+            logger.debug("push to queue {}", request.getUrl());
+            pushWhenNoDuplicate(request, task);
+        }
+    }
+
+    protected boolean shouldReserved(Request request) {
+        return request.getExtra(Request.CYCLE_TRIED_TIMES) != null;
+    }
+
+    protected boolean noNeedToRemoveDuplicate(Request request) {
+        return HttpConstant.Method.POST.equalsIgnoreCase(request.getMethod());
+    }
+
+    protected void pushWhenNoDuplicate(Request request, Task task) {
+
+    }
+}
