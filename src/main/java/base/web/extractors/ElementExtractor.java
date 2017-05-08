eaba945b7fdc1ac915c06a1bf6750548cedfb261
mzakharuk <mzakharuk@cloudlinux.com>
2017-05-08 09:28:21 +0300

    removed debug comments

--- src/main/java/base/web/extractors/ElementExtractor.java
+++ src/main/java/base/web/extractors/ElementExtractor.java
@@ -9,21 +9,8 @@ import java.util.List;
 
 public interface ElementExtractor {
 
-    /**
-     * Extract single result in text.<br>
-     * If there are more than one result, only the first will be chosen.
-     *
-     * @param element element
-     * @return result
-     */
     String select(Element element);
 
-    /**
-     * Extract all results in text.<br>
-     *
-     * @param element element
-     * @return results
-     */
     List<String> selectList(Element element);
 
 }
