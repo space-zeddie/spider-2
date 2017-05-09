734604a56574e45019f82efa6f37a7c441a30a8b
mzakharuk <mzakharuk@cloudlinux.com>
2017-05-09 12:39:43 +0300

    added new fields

--- src/main/java/base/reader/FileReader.java
+++ src/main/java/base/reader/FileReader.java
@@ -5,12 +5,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by matvii on 09.05.17.
  */
 public class FileReader implements IReader {
     private Logger logger = LoggerFactory.getLogger(getClass());
+    private Set<ResultItems> savedItems = new HashSet<ResultItems>();
+    private Set<String> checkedFilePaths = new HashSet<String>();
+
+    public FileReader() {}
 
     @Override
     public void processFile(File file) {
