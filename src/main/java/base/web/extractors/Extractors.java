f94a213b0e9814d04d352b16eeb8a3af4e094aa8
mzakharuk <mzakharuk@cloudlinux.com>
2017-05-09 09:04:58 +0300

    deprecated extractors deleted

--- src/main/java/base/web/extractors/Extractors.java
+++ src/main/java/base/web/extractors/Extractors.java
@@ -12,21 +12,13 @@ public class Extractors {
     public static RegexExtractor regex(String expr, int group) {
         return new RegexExtractor(expr,group);
     }
-    
+
     public static CssExtractor $(String expr) {
         return new CssExtractor(expr);
     }
 
     public static CssExtractor $(String expr, String attrName) {
         return new CssExtractor(expr, attrName);
-    }
-
-    public static AndExtractor and(Extractor... extractors) {
-        return new AndExtractor(extractors);
-    }
-
-    public static OrExtractor or(Extractor... extractors) {
-        return new OrExtractor(extractors);
     }
 
 }
