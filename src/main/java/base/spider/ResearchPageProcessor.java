177387724cc50496df46873d92441593d3150c38
mzakharuk <mzakharuk@cloudlinux.com>
2017-05-09 11:23:35 +0300

    renamed constants

--- src/main/java/base/spider/ResearchPageProcessor.java
+++ src/main/java/base/spider/ResearchPageProcessor.java
@@ -5,7 +5,6 @@ package base.spider;
  */
 
 import base.IPageProcessor;
-import base.utils.CharsetUtils;
 import base.utils.UrlUtils;
 import base.web.Page;
 import base.web.Site;
@@ -20,17 +19,17 @@ public class ResearchPageProcessor implements IPageProcessor {
     protected Logger logger = LoggerFactory.getLogger(getClass());
 
     private Site site = Site.instance()
-            .setRetryTimes(Constants.RETRY_TIMES)
-            .setSleepTime(Constants.SLEEP_TIME)
-            .setTimeOut(Constants.TIME_OUT);
+            .setRetryTimes(SpiderConstants.RETRY_TIMES)
+            .setSleepTime(SpiderConstants.SLEEP_TIME)
+            .setTimeOut(SpiderConstants.TIME_OUT);
 
     @Override
     public void process(Page page) {
         String charset = UrlUtils.getCharset(page.getHeaders().get("Content-Type").get(0));
         site.setCharset(charset);
-        page.addTargetRequests(page.getHtml().links().regex(Constants.PDF_LINK_PATTERN).all(), 10);
-        page.putField("paper_link", page.getUrl().regex(Constants.PDF_LINK_PATTERN).toString());
-        IExtractable regex_url = page.getUrl().regex(Constants.PDF_LINK_PATTERN);
+        page.addTargetRequests(page.getHtml().links().regex(SpiderConstants.PDF_LINK_PATTERN).all(), 10);
+        page.putField("paper_link", page.getUrl().regex(SpiderConstants.PDF_LINK_PATTERN).toString());
+        IExtractable regex_url = page.getUrl().regex(SpiderConstants.PDF_LINK_PATTERN);
         if (regex_url.toString() != null) {
             String[] parts = (regex_url.toString().split("/"));
             //System.err.println(parts[parts.length - 1]);
--- src/main/java/base/spider/SpiderConstants.java
+++ src/main/java/base/spider/SpiderConstants.java
@@ -3,7 +3,7 @@ package base.spider;
 /**
  * Created by matvii on 07.05.17.
  */
-public class Constants {
+public class SpiderConstants {
 
     public static final int RETRY_TIMES = 3;
     public static final int SLEEP_TIME = 1000;
