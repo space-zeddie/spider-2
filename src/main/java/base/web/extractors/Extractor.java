e308c232ff722bad2266f8a20e66c2a6bfd9519a
mzakharuk <mzakharuk@cloudlinux.com>
2017-05-09 09:06:23 +0300

    removed commented debug statements

--- src/main/java/base/web/extractors/Extractor.java
+++ src/main/java/base/web/extractors/Extractor.java
@@ -5,27 +5,10 @@ package base.web.extractors;
  */
 import java.util.List;
 
-/**
- * Extractor(extractor) for text.<br>
- *
- */
 public interface Extractor {
 
-    /**
-     * Extract single result in text.<br>
-     * If there are more than one result, only the first will be chosen.
-     *
-     * @param text text
-     * @return result
-     */
     String select(String text);
 
-    /**
-     * Extract all results in text.<br>
-     *
-     * @param text text
-     * @return results
-     */
     List<String> selectList(String text);
 
 }
