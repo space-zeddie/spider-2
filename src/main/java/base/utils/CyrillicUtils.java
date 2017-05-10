e1d36ccebc23a14803ec545e3eed9578f4bc98e1
mzakharuk <mzakharuk@cloudlinux.com>
2017-05-10 11:29:45 +0300

    added convertToUTF8

--- src/main/java/base/utils/CyrillicUtils.java
+++ src/main/java/base/utils/CyrillicUtils.java
@@ -1,7 +1,18 @@
 package base.utils;
 
+import java.io.UnsupportedEncodingException;
+
 /**
  * Created by matvii on 10.05.17.
  */
 public class CyrillicUtils {
+
+    public static String ISO_8859_1 = "ISO-8859-1";
+    public static String UTF_8 = "UTF-8";
+
+    public static String convertToUTF8(String cyrillic) throws UnsupportedEncodingException {
+        byte bytes[] = cyrillic.getBytes(ISO_8859_1);
+        String value = new String(bytes, UTF_8);
+        return value;
+    }
 }
